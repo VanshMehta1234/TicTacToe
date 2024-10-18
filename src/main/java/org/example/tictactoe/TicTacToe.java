9ec322320770099dbe2c98e240e07cf10ea2f558
{VanshMehta1234} <{mehta4029@gmail.com}>
2024-10-18 17:22:26 +0530

    Initial Set Up

---
+++ src/main/java/org/example/tictactoe/TicTacToe.java
@@ -0,0 +1,27 @@
+package org.example.tictactoe;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TicTacToe extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button button = new Button("Start Tic-Tac-Toe");
+        StackPane root = new StackPane(button);
+
+        Scene scene = new Scene(root, 300, 300);
+        primaryStage.setTitle("Tic-Tac-Toe");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
